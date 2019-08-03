cab1c9b5b0e9a0c0b4d6d5d0fa438e8035e7d670
gzj <981236373@qq.com>
2019-08-03 12:18:13 +0800

    number tow

---
+++ Djj.java
@@ -0,0 +1,16 @@
+public class Djj{
+    public static void main(String[] args){
+	char[] emm = {'统','计','一','个','字','符','在','字','符','串','中'};
+	for(int i=0;i<emm.length;i++){
+		if(emm[i] == '字')
+		 System.out.println(i);
+	}
+        
+	
+	   
+	}
+	
+	
+	
+	
+	}
