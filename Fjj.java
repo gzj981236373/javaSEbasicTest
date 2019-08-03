9973b8468edfd397c6bb2bd4133c1967160ed1f0
gzj <981236373@qq.com>
2019-08-03 16:32:54 +0800

    homework4

---
+++ Fjj.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Fjj{
+    public static void main(String[] args) {
+    Random random = new Random();
+	char c = (char)(int)(Math.random()*26+97);
+	System.out.println(c);
+	
+	}
+	
+}
