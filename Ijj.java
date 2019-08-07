050da4724e120d74cabc25dafcf362c15013021c
gzj <981236373@qq.com>
2019-08-07 12:01:06 +0800

    text3

---
+++ Ijj.java
@@ -0,0 +1,15 @@
+public class Ijj{
+	public static void main(String[] args){
+		for(int a = 1;a < 6;a++){
+			for (int b = 1;b < 6;b++){
+				System.out.print('*'+" ");
+			}
+			System.out.print("\n");
+		}
+		
+		
+		
+    }
+
+
+}
