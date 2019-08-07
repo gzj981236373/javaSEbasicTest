9706d1b7a496860ba9accea40fc02d413c596f9f
gzj <981236373@qq.com>
2019-08-07 12:27:50 +0800

    text4

---
+++ Jjj.java
@@ -0,0 +1,18 @@
+public class Jjj{
+	public static void main(String[] args){
+	for(int a = 1;a <= 9; a++){
+		for(int b = 1;b <= a;b++){
+			System.out.print(a+"*"+ b +"="+a*b);
+			
+		}
+
+		
+		
+	}
+   	
+	
+
+
+	
+	}
+}
