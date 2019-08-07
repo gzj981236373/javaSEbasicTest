28c7c786f92b5aa1f0d403ea772d3365a652a2a1
gzj <981236373@qq.com>
2019-08-07 16:54:10 +0800

    text8

---
+++ Qjj.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class Qjj{
+	public static void main(String[] args){
+		char[] a = {'石','原'};
+		char[] b = Arrays.copyOf(a,4);
+		System.out.print(b);
+		char[] newA = b;
+		newA[2] = '里';
+		newA[3] = '美';
+		System.out.print(newA);
+		
+		
+		
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
