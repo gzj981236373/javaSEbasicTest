27b160fc61571aff8968935569a274ef2bb9c9ef
gzj <981236373@qq.com>
2019-08-07 12:29:38 +0800

    text4

---
+++ Kjj.java
@@ -0,0 +1,27 @@
+public class Kjj{
+	public static void main(String[] args){
+	    for (int a=0;a<9;a++){
+			for (int b=0;b<9;b++){
+				if(a == 0||a == 8){
+				    System.out.print("* ");
+				}else{
+				    if(b == 0 || b == 8){
+					System.out.print("*");
+				}else{
+					System.out.print("  ");
+				} 
+				
+			}
+			
+		}
+		    System.out.println();
+		
+		
+		
+		
+	}
+	
+	
+	
+    }
+}
